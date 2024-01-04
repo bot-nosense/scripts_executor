578a1a31c4d34e566c3fb3c87700f96389aaec02
VuDoan <vudv@navisoft.com.vn>
2024-01-04 18:30:34 +0700

    Finished building basic features

--- ExcuteScripts/Main.Designer.cs
+++ ExcuteScripts/Main.Designer.cs
@@ -39,7 +39,9 @@ namespace ExcuteScripts
             this.bt_log = new System.Windows.Forms.Button();
             this.bt_submit = new System.Windows.Forms.Button();
             this.gb_stt = new System.Windows.Forms.GroupBox();
-            this.lb_stt = new System.Windows.Forms.Label();
+            this.tb_stt = new System.Windows.Forms.TextBox();
+            this.lb_Data = new System.Windows.Forms.Label();
+            this.bt_clear = new System.Windows.Forms.Button();
             this.gb_conn.SuspendLayout();
             this.gb_import.SuspendLayout();
             this.gb_event.SuspendLayout();
@@ -56,7 +58,6 @@ namespace ExcuteScripts
             this.gb_conn.TabIndex = 0;
             this.gb_conn.TabStop = false;
             this.gb_conn.Text = "Connect Database";
-            this.gb_conn.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // bt_cof
             // 
@@ -66,7 +67,6 @@ namespace ExcuteScripts
             this.bt_cof.TabIndex = 0;
             this.bt_cof.Text = "Open Config File";
             this.bt_cof.UseVisualStyleBackColor = true;
-            this.bt_cof.Click += new System.EventHandler(this.button2_Click);
             // 
             // bt_conn
             // 
@@ -76,6 +76,7 @@ namespace ExcuteScripts
             this.bt_conn.TabIndex = 0;
             this.bt_conn.Text = "Connect";
             this.bt_conn.UseVisualStyleBackColor = true;
+            this.bt_conn.Click += new System.EventHandler(this.bt_conn_Click);
             // 
             // gb_import
             // 
@@ -87,13 +88,13 @@ namespace ExcuteScripts
             this.gb_import.TabIndex = 0;
             this.gb_import.TabStop = false;
             this.gb_import.Text = "Import Files";
-            this.gb_import.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // tb_view_ip
             // 
             this.tb_view_ip.Location = new System.Drawing.Point(7, 49);
             this.tb_view_ip.Multiline = true;
             this.tb_view_ip.Name = "tb_view_ip";
+            this.tb_view_ip.ReadOnly = true;
             this.tb_view_ip.Size = new System.Drawing.Size(241, 188);
             this.tb_view_ip.TabIndex = 1;
             // 
@@ -105,6 +106,7 @@ namespace ExcuteScripts
             this.bt_import.TabIndex = 0;
             this.bt_import.Text = "Import";
             this.bt_import.UseVisualStyleBackColor = true;
+            this.bt_import.Click += new System.EventHandler(this.bt_import_Click);
             // 
             // gb_event
             // 
@@ -116,7 +118,6 @@ namespace ExcuteScripts
             this.gb_event.TabIndex = 0;
             this.gb_event.TabStop = false;
             this.gb_event.Text = "Handle";
-            this.gb_event.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // bt_log
             // 
@@ -126,7 +127,7 @@ namespace ExcuteScripts
             this.bt_log.TabIndex = 0;
             this.bt_log.Text = "Open Log File";
             this.bt_log.UseVisualStyleBackColor = true;
-            this.bt_log.Click += new System.EventHandler(this.button2_Click);
+            this.bt_log.Click += new System.EventHandler(this.bt_log_Click);
             // 
             // bt_submit
             // 
@@ -136,38 +137,59 @@ namespace ExcuteScripts
             this.bt_submit.TabIndex = 0;
             this.bt_submit.Text = "Submit";
             this.bt_submit.UseVisualStyleBackColor = true;
+            this.bt_submit.Click += new System.EventHandler(this.bt_submit_Click);
             // 
             // gb_stt
             // 
-            this.gb_stt.Controls.Add(this.lb_stt);
+            this.gb_stt.Controls.Add(this.tb_stt);
             this.gb_stt.Location = new System.Drawing.Point(13, 179);
             this.gb_stt.Name = "gb_stt";
             this.gb_stt.Size = new System.Drawing.Size(259, 77);
             this.gb_stt.TabIndex = 0;
             this.gb_stt.TabStop = false;
             this.gb_stt.Text = "Status";
-            this.gb_stt.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // lb_stt
+            // tb_stt
             // 
-            this.lb_stt.AutoSize = true;
-            this.lb_stt.Location = new System.Drawing.Point(7, 20);
-            this.lb_stt.Name = "lb_stt";
-            this.lb_stt.Size = new System.Drawing.Size(0, 13);
-            this.lb_stt.TabIndex = 0;
+            this.tb_stt.Location = new System.Drawing.Point(7, 20);
+            this.tb_stt.Multiline = true;
+            this.tb_stt.Name = "tb_stt";
+            this.tb_stt.ReadOnly = true;
+            this.tb_stt.Size = new System.Drawing.Size(246, 51);
+            this.tb_stt.TabIndex = 0;
+            // 
+            // lb_Data
+            // 
+            this.lb_Data.AutoSize = true;
+            this.lb_Data.Location = new System.Drawing.Point(13, 257);
+            this.lb_Data.Name = "lb_Data";
+            this.lb_Data.Size = new System.Drawing.Size(0, 13);
+            this.lb_Data.TabIndex = 1;
+            // 
+            // bt_clear
+            // 
+            this.bt_clear.Location = new System.Drawing.Point(449, 256);
+            this.bt_clear.Name = "bt_clear";
+            this.bt_clear.Size = new System.Drawing.Size(81, 23);
+            this.bt_clear.TabIndex = 0;
+            this.bt_clear.Text = "Clear Folder";
+            this.bt_clear.UseVisualStyleBackColor = true;
+            this.bt_clear.Click += new System.EventHandler(this.bt_clear_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 266);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(542, 283);
+            this.Controls.Add(this.lb_Data);
+            this.Controls.Add(this.bt_clear);
             this.Controls.Add(this.gb_stt);
             this.Controls.Add(this.gb_event);
             this.Controls.Add(this.gb_import);
             this.Controls.Add(this.gb_conn);
             this.Name = "Main";
             this.Text = "Auto Run Scripts";
-            this.Load += new System.EventHandler(this.Main_Load);
             this.gb_conn.ResumeLayout(false);
             this.gb_import.ResumeLayout(false);
             this.gb_import.PerformLayout();
@@ -175,6 +197,7 @@ namespace ExcuteScripts
             this.gb_stt.ResumeLayout(false);
             this.gb_stt.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -186,11 +209,13 @@ namespace ExcuteScripts
         private System.Windows.Forms.Button bt_log;
         private System.Windows.Forms.Button bt_submit;
         private System.Windows.Forms.GroupBox gb_stt;
-        private System.Windows.Forms.Label lb_stt;
         private System.Windows.Forms.TextBox tb_view_ip;
         private System.Windows.Forms.Button bt_import;
         private System.Windows.Forms.Button bt_cof;
         private System.Windows.Forms.Button bt_conn;
+        private System.Windows.Forms.TextBox tb_stt;
+        private System.Windows.Forms.Label lb_Data;
+        private System.Windows.Forms.Button bt_clear;
     }
 }
 
--- ExcuteScripts/Main.cs
+++ ExcuteScripts/Main.cs
@@ -7,29 +7,209 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System.IO;
+using System.Diagnostics;
 
 namespace ExcuteScripts
 {
     public partial class Main : Form
     {
+        private string connectionString;
+        private string dataFolderPath;
+        private string logFolder;
+        private string logFilePath;
+
         public Main()
         {
             InitializeComponent();
+            connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=ideapad3)(PORT=1522)))(CONNECT_DATA=(SID=db)));User Id=sys;Password=@nHhung123;DBA Privilege=SYSDBA;";
+            dataFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Datas");
+            logFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+            logFilePath = Path.Combine(logFolder, "log.txt");
+
+            Directory.CreateDirectory(dataFolderPath);
+            Directory.CreateDirectory(logFolder);
+            StreamWriter sw = File.CreateText(logFilePath);
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        private void LogToTextFile(string fileName, bool success)
         {
+            try
+            {
+                string logInfo = $"{DateTime.Now.ToString("yyyyMMdd-HH:mm")}, {fileName}, {(success ? "chạy thành công" : "chạy thất bại")}";
 
+                using (StreamWriter sw = File.AppendText(logFilePath))
+                {
+                    sw.WriteLine(logInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý ngoại lệ khi ghi log
+                Console.WriteLine("Error writing log: " + ex.Message);
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
 
+        private void bt_conn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OracleConnection con = new OracleConnection(connectionString))
+                {
+                    con.Open();
+                    if (con.State == System.Data.ConnectionState.Open)
+                    {
+                        tb_stt.Text = "Kết nối thành công";
+                    }
+                    else
+                    {
+                        tb_stt.Text = "Không thể kết nối";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tb_stt.Text = "Lỗi: " + ex.Message;
+            }
         }
 
-        private void Main_Load(object sender, EventArgs e)
+        private void bt_import_Click(object sender, EventArgs e)
         {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = true;
 
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    lb_Data.Text = dataFolderPath;
+                    tb_view_ip.Clear();
+
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        if (Path.GetExtension(file).Equals(".sql", StringComparison.OrdinalIgnoreCase))
+                        {
+                            try
+                            {
+                                string destinationFile = Path.Combine(dataFolderPath, Path.GetFileName(file));
+                                File.Copy(file, destinationFile, true);
+                                tb_view_ip.AppendText(Path.GetFileName(file) + Environment.NewLine);
+                            }
+                            catch (Exception ex)
+                            {
+                                tb_stt.Text = "Lỗi khi sao chép file: " + ex.Message;
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            tb_stt.Text = "Chỉ được phép import các file có đuôi là .sql";
+                        }
+                    }
+
+                    tb_stt.Text = "Đã import file.";
+                }
+            }
+        }
+
+        private void bt_submit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] sqlFiles = Directory.GetFiles(dataFolderPath, "*.sql");
+
+                foreach (string file in sqlFiles)
+                {
+                    RunSqlPlusScript(file, connectionString);
+                    string fileName = Path.GetFileName(file);
+                    LogToTextFile(fileName, true);
+                }
+                tb_stt.Text = "Chạy thành công, đã ghi vào file logs";
+            }
+            catch (Exception ex)
+            {
+                tb_stt.Text = "Error executing scripts: " + ex.Message;
+            }
+        }
+
+        private void RunSqlPlusScript(string scriptFilePath, string connectionString)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "sqlplus";
+                startInfo.Arguments = connectionString + " @" + scriptFilePath;
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.CreateNoWindow = true;
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+                    process.Start();
+
+                    // Đọc kết quả trả về từ quá trình SQLPlus
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    process.WaitForExit();
+
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        Console.WriteLine("Output: " + output);
+                    }
+
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        Console.WriteLine("Error: " + error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+
+
+        private void bt_clear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(dataFolderPath);
+
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                }
+
+                tb_stt.Text = "Đã xóa tất cả các file trong thư mục Data.";
+                tb_view_ip.Clear();
+            }
+            catch (Exception ex)
+            {
+                tb_stt.Text = "Lỗi khi xóa file: " + ex.Message;
+            }
+        }
+
+        private void bt_log_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(logFilePath);
+                tb_stt.Text = "Đang mở file ...";
+                lb_Data.Text = logFilePath;
+            }
+            catch (Exception ex)
+            {
+                tb_stt.Text = "Lỗi khi mở file log: " + ex.Message;
+            }
         }
     }
 }
+
+
+
+
