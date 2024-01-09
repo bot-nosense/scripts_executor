804348caac753525a48d3d47b3899778140a112d
VuDoan <vudv@navisoft.com.vn>
2024-01-09 16:55:33 +0700

    Constants.cs

--- ExcuteScripts/Config/Constants.cs
+++ ExcuteScripts/Config/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,8 @@ namespace ExcuteScripts.Config
         public static readonly string LOGFILEPATH = "../../Logs/log.txt";
         public static readonly string DBCONFIGPATH = "../../Config/dbConfig.txt";
         public static readonly string SYSCONFIGPATH = "../../Config/sysConfig.txt";
+        public static readonly string dataFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Datas");
+        public static readonly string logFullPath = Path.GetFullPath(LOGFILEPATH);
+        public static readonly string dbConfigFullPath = Path.GetFullPath(DBCONFIGPATH);
     }
 }
